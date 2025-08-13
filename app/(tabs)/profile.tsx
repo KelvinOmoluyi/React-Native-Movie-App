91a0c9fcaead3e7a1d573f5561d309cd4225ab7a
KelvinOmoluyi <omoluyikelvin14@gmail.com>
2025-08-13 22:51:09 +0100

    tabs group component

---
+++ app/(tabs)/profile.tsx
@@ -0,0 +1,15 @@
+//import liraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// create a component
+const Profile = () => {
+    return (
+        <View>
+            <Text>MyComponent</Text>
+        </View>
+    );
+};
+
+//make this component available to the app
+export default Profile;
---
+++ app/(tabs)/search.tsx
@@ -0,0 +1,15 @@
+//import liraries
+import React from 'react';
+import { Text, View } from 'react-native';
+
+// create a component
+const Search = () => {
+    return (
+        <View>
+            <Text>Search</Text>
+        </View>
+    );
+};
+
+//make this component available to the app
+export default Search;
